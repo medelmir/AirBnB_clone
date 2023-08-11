e75dcc86e75d630f3603485de942dbfd88906c3f
medelmir <medelmir001@gmail.com>
2023-08-11 03:25:34 -0700

    task 10

--- console.py
+++ console.py
@@ -2,9 +2,14 @@
 """cmd module"""
 
 import cmd
-import models
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,71 +28,72 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Create a new instance of BaseModel"""
+        """Create a new instance of a class"""
         if not arg:
             print("** class name missing **")
-        elif arg != "BaseModel":
+        elif arg not in globals():
             print("** class doesn't exist **")
         else:
-            from models.base_model import BaseModel
-            new = BaseModel()
-            new.save()
-            print(new.id)
+            new_instance = globals()[arg]()
+            new_instance.save()
+            print(new_instance.id)
 
     def do_show(self, arg):
-        """print the string representation of an instance"""
+        """Print the string representation of an instance"""
         args = arg.split()
         if not args:
             print("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in globals():
             print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
         else:
-            from models.base_model import BaseModel
-            from models import storage
-            key = args[0] + "." + args[1]
-            if key in storage.all():
-                print(storage.all()[key])
+            obj_key = args[0] + "." + args[1]
+            if obj_key in storage.all():
+                print(storage.all()[obj_key])
             else:
                 print("** no instance found **")
 
     def do_destroy(self, arg):
-        """delete an instance based on the class name and id"""
+        """Delete an instance based on the class name and id"""
         args = arg.split()
         if not args:
             print("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in globals():
             print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
         else:
-            from models.base_model import BaseModel
-            from models import storage
-            key = args[0] + "." + args[1]
-            if key in storage.all():
-                del storage.all()[key]
+            obj_key = args[0] + "." + args[1]
+            if obj_key in storage.all():
+                del storage.all()[obj_key]
                 storage.save()
             else:
                 print("** no instance found **")
 
     def do_all(self, arg):
-        """print all string representation of all instances"""
+        """Print all string representations of instances"""
         args = arg.split()
-        from models import storage
         if not args:
-            print([str(value) for value in storage.all().values()])
-        elif args[0] != "BaseModel":
+            obj_list = []
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+            print(obj_list)
+        elif args[0] not in globals():
             print("** class doesn't exist **")
         else:
-            print([str(value) for value in storage.all().values()])
+            obj_list = []
+            for obj in storage.all().values():
+                if type(obj).__name__ == args[0]:
+                    obj_list.append(str(obj))
+            print(obj_list)
 
     def do_update(self, arg):
-        """update an instance based on the class name and id"""
+        """Update an instance based on the class name and id"""
         args = arg.split()
         if not args:
             print("** class name missing **")
-        elif args[0] != "BaseModel":
+        elif args[0] not in globals():
             print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
@@ -96,12 +102,10 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) < 4:
             print("** value missing **")
         else:
-            from models.base_model import BaseModel
-            from models import storage
-            key = args[0] + "." + args[1]
-            if key in storage.all():
-                setattr(storage.all()[key], args[2], args[3])
-                storage.save()
+            obj_key = args[0] + "." + args[1]
+            if obj_key in storage.all():
+                setattr(storage.all()[obj_key], args[2], args[3])
+                storage.all()[obj_key].save()
             else:
                 print("** no instance found **")
 
